3c713c3212b88fc608deaab623f3f48556ca118d
Konstantin Skorupskyi <kikines@ukr.net>
2019-07-16 19:23:46 +0300

    Product details page (#35)
    * add product details page with static data
    
    * conflicts resolved 2
    
    * getting data from db
    
    * npm i
    
    * product model changed
    
    * responsive design added
    
    * return header, change currency to UAH

--- client/src/components/ProductDetails/ProductInfo.js
+++ client/src/components/ProductDetails/ProductInfo.js
@@ -8,7 +8,7 @@ class ProductInfo extends Component {
         return (
             <>
             <p className="info-block__title">{data.title}</p>
-            <p className="info-block__price">${data.price}</p>
+            <p className="info-block__price"> &#8372;{data.price}</p>
             <p className="info-block__description">{data.description}</p>
             <p className="info-block__brand"> - Бренд : {data.brand}</p>
             <p className="info-block__code"> - Артикул : {data.code}</p>
