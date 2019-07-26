c2b48af6198e9af1633158068d2bfa21095b83aa
Konstantin Skorupskyi <kikines@ukr.net>
2019-07-26 17:00:55 +0300

    fixed bug of same items in a cart  (#53)
    * add product details page with static data
    
    * conflicts resolved 2
    
    * getting data from db
    
    * npm i
    
    * product model changed
    
    * responsive design added
    
    * return header, change currency to UAH
    
    * fix request path
    
    * added functional to ToCartButton, added input to enter the quantity of product, added modal if choosed quantity higher than availability
    
    * quantity bug fixed
    
    * fixed bug with multiply items of same type in a cart

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -4,47 +4,65 @@ import {
   DECREMENT_CART_ITEM,
   DELETE_CART_ITEM,
   ADD_CART_ITEM
+<<<<<<< HEAD
+} from "../actions/types";
+=======
   } from "../actions/types";
+>>>>>>> develop
 
 const initialState = [];
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case GET_LOCAL_STORAGE_CART:
-      return action.payload;   
+      return action.payload;
 
     case INCREMENT_CART_ITEM: {
       const newArj = state.map(item => {
         if (item.code === action.payload) {
-          if (item.quantity < item.availability)          
-          return {...item, quantity: item.quantity + 1};
+          if (item.quantity < item.availability)
+            return { ...item, quantity: item.quantity + 1 };
           else return item;
         }
-        else return item;      
+        else return item;
       });
-      return newArj;      
+      return newArj;
     }
 
     case DECREMENT_CART_ITEM: {
       const newArj = state.map(item => {
         if (item.code === action.payload) {
-          if (item.quantity > 1) return {...item, quantity: item.quantity - 1};
+          if (item.quantity > 1) return { ...item, quantity: item.quantity - 1 };
           else return item;
         }
-        else return item;      
+        else return item;
       });
-      return newArj;      
-    }     
-    case DELETE_CART_ITEM: {      
+      return newArj;
+    }
+    case DELETE_CART_ITEM: {
       const newArj = state.filter(item => {
-        return (item.code != action.payload);        
+        return (item.code != action.payload);
       });
+<<<<<<< HEAD
+      return newArj;
+    }
+    case ADD_CART_ITEM: {
+      const newArj = state.filter(item => {
+        if(item.code === action.payload.code) {
+          action.payload.quantity += item.quantity
+        } 
+        return item.code != action.payload.code
+      })
+=======
       return newArj;      
     }     
     case ADD_CART_ITEM: {
       return [...state, action.payload]
     }
+>>>>>>> develop
 
+      return [...newArj, action.payload]
+    }
     default: return state;
   }
 }
