a23c8d941c2808023a35bc983bab515cc00a8742
Krechetov Yevhenii <jenya-99@ukr.net>
2019-09-16 22:32:43 +0300

    Edited path

--- server/server.js
+++ server/server.js
@@ -52,8 +52,8 @@ app.use('/', require('./api'));
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
   });
 
 
