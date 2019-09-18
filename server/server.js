5432f1f70e4454df16c8c0b3de62937460a7e748
Krechetov Yevhenii <jenya-99@ukr.net>
2019-09-18 23:53:53 +0300

    fixed path

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use(express.static('../client/build'));
 
 app.get('*', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    res.sendFile('client/build/index.html');
+    res.sendFile('../client/build/index.html');
   });
 
 
