06cf9135cf0e774186ea854886f168aad7441b80
Krechetov Yevhenii <jenya-99@ukr.net>
2019-09-23 15:28:14 +0300

    edited pathes

--- server/server.js
+++ server/server.js
@@ -50,12 +50,14 @@ db.on('error', function (err) {
 app.use(bodyParser.json());
 app.use('/', require('./api'));
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 // app.use(express.static('../client/build'));
+app.use(express.static('../serever/build'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     // res.sendFile('../client/build/index.html');
+    res.sendFile('../server/build/index.html');
   });
 
 
